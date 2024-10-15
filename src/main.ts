868f50e8b9cef386876b12f610581ac0a906ce8d
Rishabh Raghuwanshi <Rishabhhsraghuwanshi@gmail.com>
2024-10-15 22:12:30 +0530

    fix prisma outdated issue

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors()
   app.useGlobalPipes(new ValidationPipe())
   const config = new DocumentBuilder()
       .setTitle(' Todo REST API')
@@ -14,7 +15,13 @@ async function bootstrap() {
       .addTag('todos')
       .build();
     const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('api', app, document);
+    SwaggerModule.setup('/Swagger', app, document,{
+      customCssUrl:
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      customJs:[
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js'
+      ],
+    });
   await app.listen(3000);
 }
 bootstrap();
