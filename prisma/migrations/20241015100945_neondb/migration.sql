a892ef8d44150eeac67f0166bc23ec531969ac12
Rishabh Raghuwanshi <Rishabhhsraghuwanshi@gmail.com>
2024-10-17 00:50:41 +0530

    prisma migrate to remove last migration

--- prisma/migrations/20241015100945_neondb/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[userEmail]` on the table `Todo` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Todo_userEmail_key" ON "Todo"("userEmail");
